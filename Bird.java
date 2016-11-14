76986846934e5c78f8fca75cd8ee9f87396ce8cd
vinnikoff <winr7post@gmail.com>
2016-11-14 21:10:57 +0200

    project3-1

---
+++ Bird.java
@@ -0,0 +1,13 @@
+/**
+ * Created by VINNI on 13.11.16.
+ */
+public class Bird {
+
+String say;
+
+    public void sing ()
+    {
+        System.out.println(say);
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by VINNI on 13.11.16.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Bird bird = new Bird();
+        bird.say = "I am walking";
+        bird.sing();
+        bird.say = "I am flying";
+        bird.sing();
+        bird.say = "I am singing";
+        bird.sing();
+        bird.say = "I am Bird";
+        bird.sing();
+    }
+
+}
+
+
+
+
